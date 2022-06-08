76c0b2091e0f54aa0546ef84a05805a2defa3701
yihang zhao <yihangz@kth.se>
2022-06-09 01:21:23 +0200

    change the priority

---
+++ vep_1/partition_0_3/main.d
@@ -0,0 +1,9 @@
+main.o: main.c ../../vep_0/libbsp/include/xil_printf.h \
+ ../../vep_0/libbsp/include/xil_types.h \
+ ../../vep_0/libbsp/include/platform.h
+
+../../vep_0/libbsp/include/xil_printf.h:
+
+../../vep_0/libbsp/include/xil_types.h:
+
+../../vep_0/libbsp/include/platform.h:
